ed8938ef9c6fe73837d8379ecff596f4a2c65ad4
Heather Beckman <heatherbeckman85@gmail.com>
2020-10-07 14:40:53 -0600

    made toggle work with usedarkmode, mvp

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -4,6 +4,8 @@ import {useState} from 'react';
 const useDarkMode = (key, initialValue) => {
 
     const [value, setValue] = useLocalStorage(key, initialValue);
+
+    return [value, setValue];
 };
 
 export default useDarkMode;
